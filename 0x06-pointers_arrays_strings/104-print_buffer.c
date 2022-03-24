1673dc6efba6b65361ed64fdb55226eda4d4f8b0
wmoyikwa <99610930+wmoyikwa@users.noreply.github.com>
2022-03-24 23:12:19 +0200

    Update 104-print_buffer.c
    my commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -14,29 +14,38 @@ void print_buffer(char *b, int size)
 	for (x = 0; x < size; x += 10)
 	{
 		printf("%08x: ", x);
+
 		for (i = 0; i < 10; i++)
 		{
 			if ((i + x) >= size)
 				printf("  ");
+
 			else
-				printf("%02x", *(b + i + x));   
+				printf("%02x", *(b + i + x));
+
 			if ((i % 2) != 0 && i != 0)
 				printf(" ");
 		}
+
 		for (i = 0; i < 10; i++)
 		{
 			if ((i + x) >= size)
 				break;
+
 			else if (*(b + i + x) >= 31 &&
 				 *(b + i + x) <= 126)
 				printf("%c", *(b + i + x));
+
 			else
 				printf(".");
 		}
+
 		if (x >= size)
 			continue;
+
 		printf("\n");
 	}
+
 	if (size <= 0)
 		printf("\n");
 }
